38b64a1ce10f8220a65dc6e0acdcd9ffa130c0f9
zakilive <iammzaki@gmail.com>
2015-12-27 17:40:09 +0600

    php-gcm project that can send message to destination number using google cloud messaging.

---
+++ gcmtest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+author:Syed Ahmed Zaki
+blog:www.zakilive.com
+facebook.com/zakilivebuzz
+*/
+
+// API access key from Google API's Console
+define( 'API_ACCESS_KEY', 'AIzaSyBpP2Dve2Io17gVB-UDwOmhMLHXoX1DVCo' );
+$registrationIds = array( $_GET['id'] ); //it is getting registration id from the index page.
+
+// prep the bundle.You can define the desired values from gcm library.I needed message and number only so it has defined here.
+$msg = array
+(
+	'message' 	=> $_GET['mess'],
+	'number'	=> $_GET['numb'],
+);
+//It is json passing
+$fields = array
+(
+	'registration_ids' 	=> $registrationIds,
+	'data'			=> $msg,
+);
+ 
+$headers = array
+(
+	'Authorization: key=' . API_ACCESS_KEY,
+	'Content-Type: application/json'
+);
+ 
+$ch = curl_init();
+curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
+curl_setopt( $ch,CURLOPT_POST, true );
+curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+$result = curl_exec($ch );
+curl_close( $ch );
+echo $result;
---
+++ index.php
@@ -0,0 +1,31 @@
+<!-- 
+author:Syed Ahmed Zaki
+blog:www.zakilive.com
+facebook.com/zakilivebuzz 
+-->
+
+<html>
+<head>
+<title>SMS Gateway</title>
+</head>
+
+<body>
+<form action="gcmtest.php" method="GET">
+<label>PHP-GCM<br>
+  <br>
+  Mobile Nummer</label>
+<br>
+<input type="text" name="numb">
+<br>
+<label>Message</label>
+<br>
+<textarea name="mess"></textarea>
+<br>
+<br>
+<textarea name="id">registration id from your device</textarea>
+<br>
+<input type="submit" name="btn" value="SUBMIT">
+</form>
+</body>
+
+</html>
